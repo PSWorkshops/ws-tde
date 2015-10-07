b1a7fe4594c921fa07fc00e8b7fbdf4486855b27
Ted Coyle <username@users.noreply.github.com>
2015-10-07 08:42:10 -0400

    test

--- x.sh
+++ x.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Environment setting script.
 # don't use .oraenv don't use oraenv don't use oraenv
+# ---------------------------------------------------
 
 source ./setkeydb.sh keydb1
 lsnrctl $1
